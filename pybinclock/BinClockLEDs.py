0f42eb22e2588feff69474e39fa24a008f94e11c
Robert Sigler <robert@sigler.io>
2022-10-06 15:00:33 -0400

    Adding LED code

---
+++ pybinclock/BinClockLEDs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from colorsys import hsv_to_rgb
+from unicornhatmini import UnicornHATMini
+from time import sleep, time
+from PyBinClock import CurrentTime
+
+unicornhatmini = UnicornHATMini()
+unicornhatmini.set_brightness(0.1)
+unicornhatmini.set_rotation(180)
+width, height = unicornhatmini.get_shape()
+
+field = [[0 for x in range(width)] for y in range(height)]
+
+ct = CurrentTime()
+try:
+    while True:
+        ct.update()
+        # print(ct.now)
+        row = 0
+        for i in [
+            ct.binary['year'],
+            ct.binary['month'],
+            ct.binary['day'],
+            ct.binary['hour'],
+            ct.binary['minute'],
+            ct.binary['second']
+        ]:
+            lastLED = 0
+            for led in range(len(i)):
+                if i[led] == 1:
+                    hue = (time() / 10.0)
+                    field[row][led] = [int(c * 255)
+                                       for c in hsv_to_rgb(hue, 1.0, 1.0)]
+                else:
+                    field[row][led] = (0, 0, 0)
+                unicornhatmini.set_pixel(led + len(i), row, *field[row][led])
+                #print(row, 16 - led, *field[row][led])
+                lastLED = led
+            #unicornhatmini.set_pixel(lastLED + 1, row, 255, 255, 255)
+            row = row + 1
+        unicornhatmini.show()
+        sleep(1)
+except KeyboardInterrupt:
+    raise SystemExit
