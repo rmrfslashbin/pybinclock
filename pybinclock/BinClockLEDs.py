5dbf9c9f75a483a509e3a5b759de100537e78568
Robert Sigler <robert@sigler.io>
2022-10-07 13:41:55 -0400

    Adding LED Controller class

--- pybinclock/BinClockLEDs.py
+++ pybinclock/BinClockLEDs.py
@@ -3,22 +3,55 @@
 from colorsys import hsv_to_rgb
 from unicornhatmini import UnicornHATMini
 from time import sleep, time
+from loguru import logger
 from pybinclock.PyBinClock import CurrentTime
 
 
+class LEDController:
+    def __init__(self, rotation: int = 0, brightness: float = 0.1) -> None:
+        self.hat = UnicornHATMini()
+        self.hat.set_brightness(brightness)
+        self.hat.set_rotation(rotation)
+        self.width, self.height = self.hat.get_shape()
+        self.field = []
+
+        self.OKAY = [0, 255, 0]
+        self.ERROR = [255, 0, 0]
+        self.WARN = [255, 255, 0]
+        self.INFO = [0, 0, 255]
+
+        self.status = {}
+        self.status['okay'] = [0, 6, self.INFO]  # x, y, [r, g, b]
+
+        self.reset()
+
+    def reset(self) -> None:
+        # Initialize the display
+        # Initialize the display
+        self.field = [
+            [[0, 0, 0] for x in range(17)]
+            for y in range(7)
+        ]
+        self.draw()
+
+    def draw(self) -> None:
+        for s in self.status:
+            self.field[self.status[s][1]
+                       ][self.status[s][0]] = self.status[s][2]
+
+        for yndx, y in enumerate(self.field):
+            for xndx, x in enumerate(y):
+                print(xndx, yndx, *x)
+                self.hat.set_pixel(xndx, yndx, *x)
+
+        self.hat.show()
+
+
+@logger.catch
 def BinClockLEDs():
-    print("Starting BinClockLEDs")
-    # Set up the Unicorn HAT Mini
-    unicornhatmini = UnicornHATMini()
-    unicornhatmini.set_brightness(0.1)
-    # Rotate the display 180 degrees
-    unicornhatmini.set_rotation(180)
+    logger.info("Starting BinClockLEDs")
 
-    # Get the width and high of the display
-    width, height = unicornhatmini.get_shape()
-
-    # Initialize the display
-    field = [[0 for x in range(width)] for y in range(height)]
+    leds = LEDController(rotation=180)
 
     # Initialize the current time
     ct = CurrentTime()
@@ -56,22 +89,23 @@ def BinClockLEDs():
                         # rgb = [int(c * 255) for c in hsv_to_rgb(hue, 1.0, 1.0)]
 
                         rgb = [255, 0, 0]
-                        field[row][led] = rgb
+                        leds.field[row][16 - led] = rgb
                     else:
                         # Turn off the LED
-                        field[row][led] = (0, 0, 0)
+                        leds.field[row][16 - led] = [0, 0, 0]
 
                     # Set the LED (on or off) and shift light to the right (little endian)
-                    unicornhatmini.set_pixel(16 - led, row, *field[row][led])
+                    #unicornhatmini.set_pixel(16 - led, row, *field[row][led])
                     #lastLED = led
                 # unicornhatmini.set_pixel(lastLED + 1, row, 255, 255, 255)
                 row = row + 1
             # Redraw the display
-            unicornhatmini.show()
+            # unicornhatmini.show()
+            leds.draw()
             sleep(1)
             # print()
     except KeyboardInterrupt:
-        print("Exiting BinClockLEDs")
+        logger.info("Exiting BinClockLEDs")
         raise SystemExit
 
 
