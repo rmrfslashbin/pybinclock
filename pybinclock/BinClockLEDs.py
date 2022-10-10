f55385b16e284133ebbc118870cb4dc48f6e8555
Robert Sigler <robert@sigler.io>
2022-10-10 11:56:19 -0400

    Adding a pause button

--- pybinclock/BinClockLEDs.py
+++ pybinclock/BinClockLEDs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from colorsys import hsv_to_rgb
+from gpiozero import Button
 from unicornhatmini import UnicornHATMini
 from time import sleep, time
 from loguru import logger
@@ -8,6 +9,17 @@ from pybinclock.PyBinClock import CurrentTime
 
 
 class LEDController:
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.hat.clear()
+        self.hat.show()
+        self.button_a.close()
+        self.button_b.close()
+        self.button_x.close()
+        self.button_y.close()
+
     def __init__(self, rotation: int = 0, brightness: float = 0.1) -> None:
         self.hat = UnicornHATMini()
         self.hat.set_brightness(brightness)
@@ -20,13 +32,22 @@ class LEDController:
         self.WARN = [255, 255, 0]
         self.INFO = [0, 0, 255]
 
+        self.button_a = Button(5)
+        self.button_b = Button(6)
+        self.button_x = Button(16)
+        self.button_y = Button(24)
+
+        self.button_a.when_pressed = self.togglePause
+
+        self.paused = False
+
         self.status = {}
         self.status['okay'] = [0, 6, self.INFO]  # x, y, [r, g, b]
+        self.status['paused'] = [1, 6, self.OKAY]  # x, y, [r, g, b]
 
         self.reset()
 
     def reset(self) -> None:
-        # Initialize the display
         # Initialize the display
         self.field = [
             [[0, 0, 0] for x in range(17)]
@@ -34,17 +55,36 @@ class LEDController:
         ]
         self.draw()
 
+    def setStatus(self, status: str, color: list) -> None:
+        if status == 'okay':
+            self.status['okay'] = [0, 6, color]  # x, y, [r, g, b]
+
+        if status == 'paused':
+            logger.info('Setting paused')
+            self.status['paused'] = [1, 6, color]
+
+        self.draw()
+
     def draw(self) -> None:
         for s in self.status:
             self.field[self.status[s][1]
                        ][self.status[s][0]] = self.status[s][2]
 
-        for yndx, y in enumerate(self.field):
-            for xndx, x in enumerate(y):
-                print(xndx, yndx, *x)
-                self.hat.set_pixel(xndx, yndx, *x)
+        for yndx, row in enumerate(self.field):
+            for xndx, rgb in enumerate(row):
+                self.hat.set_pixel(xndx, yndx, *rgb)
 
         self.hat.show()
+
+    def togglePause(self) -> None:
+        self.paused = not self.paused
+
+        if self.paused:
+            logger.info('Paused')
+            self.setStatus('paused', self.ERROR)
+        else:
+            logger.info('Unpaused')
+            self.setStatus('paused', self.OKAY)
 
 
 @logger.catch
@@ -60,9 +100,13 @@ def BinClockLEDs():
     try:
         # loop forever
         while True:
+            if leds.paused:
+                continue
             # Update the current time
             ct.update()
             # print(ct.now)
+
+            leds.setStatus('okay', leds.OKAY)
 
             # Reset the row int to 0
             row = 0
